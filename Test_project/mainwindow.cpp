4d1787bb4753f7f24e403771ff69f03afc984c87
sandeep singh <sandeep singh>
2019-10-25 16:42:35 +0530

    Single Tab Code

---
+++ Test_project/mainwindow.cpp
@@ -0,0 +1,219 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+
+
+void MainWindow::on_GetInfo_clicked()
+{
+
+}
+
+void MainWindow::on_lineEdit_2_cursorPositionChanged(int arg1, int arg2)
+{
+
+}
+
+void MainWindow::on_ShutDown_clicked()
+{
+
+}
+
+void MainWindow::on_Statusinfo_clicked()
+{
+
+}
+
+void MainWindow::on_EventLog_clicked()
+{
+
+}
+
+void MainWindow::on_FormatRSA_clicked()
+{
+
+}
+
+void MainWindow::on_Reset_clicked()
+{
+
+}
+
+void MainWindow::on_CurrentIPAddress_clicked()
+{
+
+}
+
+void MainWindow::on_Quit_clicked()
+{
+
+}
+
+void MainWindow::on_pushButton_9_clicked()
+{
+
+}
+
+void MainWindow::on_Date_clicked()
+{
+
+}
+
+void MainWindow::on_PresentDirectory_clicked()
+{
+
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+
+}
+
+void MainWindow::on_Version_clicked()
+{
+
+}
+
+void MainWindow::on_SizeofDisk_clicked()
+{
+
+}
+
+void MainWindow::on_ShowChannelFault_clicked()
+{
+
+}
+
+void MainWindow::on_ChannelsReset_clicked()
+{
+
+}
+
+void MainWindow::on_RSAMEDIA_clicked()
+{
+
+}
+
+void MainWindow::on_SaveEventLog_clicked()
+{
+
+}
+
+void MainWindow::on_BIT_clicked()
+{
+
+}
+
+void MainWindow::on_frame_customContextMenuRequested(const QPoint &pos)
+{
+
+}
+
+void MainWindow::on_pushButton_2_clicked()
+{
+
+}
+
+void MainWindow::on_pushButton_3_clicked()
+{
+
+}
+
+void MainWindow::on_CommandsPostData_customContextMenuRequested(const QPoint &pos)
+{
+
+}
+
+
+
+void MainWindow::on_plainTextEdit_blockCountChanged(int newBlockCount)
+{
+
+}
+
+void MainWindow::on_frame_4_customContextMenuRequested(const QPoint &pos)
+{
+
+}
+
+void MainWindow::on_frame_3_customContextMenuRequested(const QPoint &pos)
+{
+
+}
+
+void MainWindow::on_frame_2_customContextMenuRequested(const QPoint &pos)
+{
+
+}
+
+void MainWindow::on_frame_4_windowTitleChanged(const QString &title)
+{
+
+}
+
+void MainWindow::on_frame_6_customContextMenuRequested(const QPoint &pos)
+{
+
+}
+
+void MainWindow::on_frame_5_customContextMenuRequested(const QPoint &pos)
+{
+
+}
+
+void MainWindow::on_pushButton_4_clicked()
+{
+
+}
+
+void MainWindow::on_pushButton_5_clicked()
+{
+
+}
+
+void MainWindow::on_Timeset_clicked()
+{
+
+}
+
+void MainWindow::on_mv_clicked()
+{
+
+}
+
+void MainWindow::on_remdir_clicked()
+{
+
+}
+
+void MainWindow::on_rmdirlineEdit_cursorPositionChanged(int arg1, int arg2)
+{
+
+}
+
+void MainWindow::on_SourcelineEdit_cursorPositionChanged(int arg1, int arg2)
+{
+
+}
+
+void MainWindow::on_DestnlineEdit_cursorPositionChanged(int arg1, int arg2)
+{
+
+}
+
+void MainWindow::on_timeEdit_userTimeChanged(const QTime &time)
+{
+
+}
